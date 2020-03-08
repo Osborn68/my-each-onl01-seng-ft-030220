0a795cabda20a34fc78a29579f9f4b1f41edf10a
Christopher Osborn <z68ozz@gmail.com>
2020-03-08 19:00:25 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,12 @@
-def my_each(array)
-  new_array=[]
-  greetings = array.size
-  
-  while new_array < greetings
-  ""
+def my_find(collection)
+  i=0
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    end
+    i += 1
+  end
 end
+
+nums = (1..100).to_a
+my_find(nums){|i| i % 71 == 0}
